0b4ad6536193599d93150d35e5fa1a080c7f0697
noahzuo <noahzuo@163.com>
2023-05-22 20:58:58 +0800

    Oops

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-import os
-
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        user_input = request.form["user_input"]
+        response = generate_response(user_input)
+        return jsonify({"response": response})
+    return render_template("index.html")
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+def generate_response(user_input):
+    # 在这里，你可以使用GPT模型生成回复。为简化示例，我们将返回一个简单的字符串。
+    return f"你好！你刚才说了：{user_input}"
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+if __name__ == "__main__":
+    app.run(debug=True)
